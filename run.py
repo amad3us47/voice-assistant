ec0738a2f3d966057d12beeb3b769f58b330a50c
Aman <131179388+amad3us47@users.noreply.github.com>
2023-08-19 23:57:22 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 import openai
-from AppOpener import open
-from AppOpener import close
 import pyttsx3
 import os
-import pandas as pd
 import time
 import speech_recognition as sr
-engine=pyttsx3.init()
-newVoiceRate=130
-engine.setProperty('rate',newVoiceRate)
-openai.api_key = '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'
+from AppOpener import open
+from AppOpener import close
+
+# Initialize text-to-speech engine
+engine = pyttsx3.init()
+newVoiceRate = 130
+engine.setProperty('rate', newVoiceRate)
+
+# Set your OpenAI API key
+openai.api_key = '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'
+
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
@@ -18,31 +22,45 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         temperature=0,
     )
     return response.choices[0].message["content"]
-r=sr.Recognizer()
-text="hello"
+
+r = sr.Recognizer()
+
 while True:
-    with sr.Microphone(device_index=1) as source:
-        print("Hello i m jarvis your virtual assistant")
-        r.adjust_for_ambient_noise(source)
-        audio= r.listen(source)
-        text = r.recognize_google(audio)
-        text_split=text.split()
-        print(text_split)
-        for x in text_split:
-            if(x=="search"):
-                respone=get_completion(text)
-                engine.say(respone)
+    with sr.Microphone() as source:
+        print("Hello, I'm Jarvis, your virtual assistant")
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+        
+        try:
+            text = r.recognize_google(audio)
+            text = text.lower()  # Convert to lowercase for easier comparison
+            print("Recognized:", text)
+            
+            if "search" in text:
+                response = get_completion(text)
+                print("Response:", response)
+                engine.say(response)
                 engine.runAndWait()
-            if(x=="shutdown"):
-                engine.say("system is going offline ")
+
+            elif "shutdown" in text:
+                engine.say("System is going offline")
                 engine.runAndWait()
                 time.sleep(5)
                 os.system('shutdown -s')
-            if(x=="open" and "whatsapp"):
+
+            elif "open whatsapp" in text:
+                print("Opening WhatsApp")
+                # Implement your logic to open WhatsApp here
                 open("whatsapp")
-            if(x=="close" and "whatsapp"):
+
+            elif "close whatsapp" in text:
+                print("Closing WhatsApp")
+                # Implement your logic to close WhatsApp here
                 close("whatsapp")
             else:
-                respone=get_completion(text)
-    if text_split[0]=="stop":
-        break
+                print("Unknown command")
+
+        except Exception:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
